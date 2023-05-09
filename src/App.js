01c20c2fdd99eee0dbd7df07f05dad04d183e5e9
heetaehwang <tejaia@naver.com>
2023-05-10 00:12:46 +0900

    Write UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Map from './components/map/Map';
+import Button from './components/ui/Button';
+import Inputbox from './components/ui/Inputbox';
 function App() {
   return (
     <div>
+      <div>
+        <Button/>
+        <Inputbox/>
+      </div>
       <Map/>
     </div>
 
