70b774b5c5740f57de78f2dd064c39d5536c0410
DESKTOP-97R11IF\HWANG2 <tejaia@naver.com>
2023-05-26 16:17:01 +0900

    lan lon transmit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Home from "./components/pages/home";
-import firebase from 'firebase/app';
-import 'firebase/database';
+import { firestore } from "./firebase";
 function App(){
-  const database =firebase.database();
-  database.ref('/path/to/data').once('value')
-  .then((snapshot) => {
-    const data =snapshot.val();
-    console.log(data);
-
+  const lat = [];
+  const lon = [];
+  let a = 0;
+  firestore.collection('health').get().then((결과)=>{
+    결과.forEach((doc) => {
+      lat.push(doc.data().위도);
+      lon.push(doc.data().경도);
+      console.log(lat[a]);
+      a++;
+    })
   })
-  .catch((error) => {
-    console.error(error);
-  });
   return (
   <div>
     <Home/>
