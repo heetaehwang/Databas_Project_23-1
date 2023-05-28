e715f9b0ad14a5a64678f90ddb9d4f7277f1f109
heetaehwang <tejaia@naver.com>
2023-05-29 04:07:24 +0900

    change Home, App CSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import Home from "./components/pages/home";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import ListWrite from "./components/pages/listWrite";
+import styled from "styled-components";
+
+const MainTitleText = styled.p`
+    font-size: 96px;
+    font-weight: bold;
+    text-align: center;
+`;
 function App(){
 
   return (
-    <BrowserRouter>
+    <BrowserRouter>               
+      <MainTitleText>...20Kg...</MainTitleText>
       <Routes>
         <Route index element ={<Home/>}/>
         <Route path="list-write" element = {<ListWrite/>}/>
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,48 +1,38 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import Map from "../map/map";
 import ListShow from "../pages/listShow"
-// const Title = styled.div`
-//     padding: 10px;
-//     border: 1px solid balck;
-//     width: 720px;
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     justify-content: center;
-// `;
-// const Wrapper = styled.div`
-//     padding: 16px;
-//     width: calc(100% - 32px);
-//     border: 1px solid balck;
 
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     justify-content: center;
-// `;
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
 
-// const Container = styled.div`
-//     width: 100%;
-//     max-width: 720px;
-//     border: 1px solid balck;
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
 
-//     & > * {
-//         :not(:last-child) {
-//             margin-bottom: 16px;
-//         }
-//     }
-// `;
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
 
 function Home(props) {
 
     return (
-
-        <div>
-                <h1>...20Kg...</h1>
+        <Wrapper>
+            <Container>
                 <ListShow/>
                 <Map/>
-        </div>
+            </Container>
+        </Wrapper>
+
     );
 }
 
